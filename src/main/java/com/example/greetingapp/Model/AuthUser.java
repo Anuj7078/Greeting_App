157b4dfa448d6217bc3b6cef137f9184c2124f5e
Anuj Choudhary <anuj.choudhary.2711@gmail.com>
2025-03-04 11:47:35 +0530

    UC9 committed

--- src/main/java/com/example/greetingapp/Model/AuthUser.java
+++ src/main/java/com/example/greetingapp/Model/AuthUser.java
@@ -1,4 +1,29 @@
+//This is UC9 - Model to stores details
 package com.example.greetingapp.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "auth_user")
 public class AuthUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
